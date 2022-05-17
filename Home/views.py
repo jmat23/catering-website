1b05683f4ea4697848dff76844d978bb9241dba2
Luis Reyes <reyesluichi@hotmail.com>
2022-05-17 13:16:02 +1000

    got home working with test html file

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def tests(request):
-	return HttpResponse("ok")
+	return render(request, 'home.html')
+	
--- cateringWebsite/urls.py
+++ cateringWebsite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("orders.urls")),
+    path('orders/', include("orders.urls")),
+    path('', include("Home.urls")),
 ]
