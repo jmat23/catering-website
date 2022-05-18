ebe10f316d32362d7851b01093ed344553093d82
Luis Reyes <reyesluichi@hotmail.com>
2022-05-18 17:53:13 +1000

    mock menu working but no style

---
+++ orders/testmenu.py
@@ -0,0 +1,16 @@
+def generate():
+    test = [
+        {
+            "name": "Sisig",
+            "stock": [1, 2, 2]
+        },
+        {
+            "name": "Palabok",
+            "stock": [0, 1, 5]
+        },
+        {
+            "name": "Lasagne",
+            "stock": [0, 0, 0]
+        }
+    ]
+    return test
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from orders.testmenu import generate
 
 # Create your views here.
 def tests(request):
-	return render(request, 'orders.html')
+	menu = generate()
+	return render(request, 'orders.html', {'menu': menu})
